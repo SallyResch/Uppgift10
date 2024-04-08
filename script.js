c9462c8ccb47c05cd0e42418b1f1bff41386dca6
Sillysally <92034603+SallyResch@users.noreply.github.com>
2024-04-08 10:00:31 +0200

    added basic javascript file and html file

---
+++ script.js
@@ -0,0 +1,10 @@
+function getApi(){ 
+    /*Skriv din kod här*/ 
+    fetch(fullUri) 
+    .then(res => res.json()) 
+    .then(data => 
+    { 
+    /*Och här*/ 
+    }) 
+    .catch(err => console.log(err)) 
+    }
