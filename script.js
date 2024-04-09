84a6debee3c9145a4220552932242ead32b15d1b
Sillysally <92034603+SallyResch@users.noreply.github.com>
2024-04-09 09:59:56 +0200

    made pic show but not in correct way

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-function getApi(){ 
-    /*Skriv din kod här*/ 
-    fetch(fullUri) 
-    .then(res => res.json()) 
-    .then(data => 
-    { 
-    /*Och här*/ 
-    }) 
-    .catch(err => console.log(err)) 
-    }
+document.addEventListener('DOMContentLoaded',function(){
+  const baseUrl='https://deckofcardsapi.com/api/deck/new/draw/?count=1';
+  const showButton = document.querySelector('#showButton');
+  const cardDiv = document.querySelector('#cardDiv');
+
+  const getCard =()=>{
+    fetch(baseUrl,{
+      method: 'GET',
+      headers: {
+        'Accept': 'image/*'
+      }
+    })
+    .then(res => {
+      if (res.ok) 
+        return res.blob();
+      throw new Error('Failed to get card');
+    })
+    .then(blob=>{
+      var imageUrlObject = URL.createObjectURL(blob);
+      var imageElement = document.createElement('img');
+      imageElement.src=imageUrlObject;
+      document.body.appendChild(imageElement);
+    })
+    .catch(error => {
+      console.error('Det uppstod ett fel:', error);
+  })}
+  showButton.addEventListener('click', getCard);
+})
